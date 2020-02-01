b387f28daedd28b7031a60b10f7cf36a97795551
zen-yang <519796642@qq.com>
2020-02-01 19:28:15 +0800

    重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>Labels.vue
-    <Nav />
+  <div class="nav-wrapper">
+    <div class="content">
+      Labels.vue
+    </div>
+    <Nav/>
   </div>
 </template>
 
@@ -11,5 +14,16 @@
 </script>
 
 <style lang="scss" scoped>
+  .nav-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
 
+  .content {
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
