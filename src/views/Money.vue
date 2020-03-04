c53a9e0bbf22382e8f2296f8bd90ab359cebadea
zen-yang <519796642@qq.com>
2020-03-05 00:06:44 +0800

    可以选中或取消 tag

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,19 +3,25 @@
     <NumberPad/>
     <Types/>
     <Notes/>
-    <Tags/>
+    <Tags :data-source="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
-  import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
-  import Tags from '@/components/Money/Tags.vue';
+<script>
+  import NumberPad from '@/components/Money/NumberPad.vue'
+  import Types from '@/components/Money/Types.vue'
+  import Notes from '@/components/Money/Notes.vue'
+  import Tags from '@/components/Money/Tags.vue'
+
   export default {
     name: 'Money',
-    components: { Tags, Notes, Types, NumberPad },
-  };
+    components: {Tags, Notes, Types, NumberPad},
+    data() {
+      return {
+        tags: ['衣', '食', '住', '行', '彩票']
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
