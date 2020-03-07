8aaa548d86c2b48b874d398d6cd1a25ec8cce3ff
zen-yang <519796642@qq.com>
2020-03-07 18:43:24 +0800

    完成 CSS 提取了几个通用组件

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label class="notes">
+    <label class="formItem">
       <span class="name">{{this.fileName}}</span>
       <input type="text"
              v-model="value"
@@ -14,7 +14,7 @@
   import { Component, Prop, Watch } from 'vue-property-decorator';
 
   @Component
-  export default class Notes extends Vue {
+  export default class FormItem extends Vue {
     value = '';
 
     @Prop({required: true}) fileName!: string;
@@ -28,9 +28,8 @@
 </script>
 
 <style lang="scss" scoped>
-  .notes {
+  .formItem {
     font-size: 14px;
-    background: #f5f5f5;
     padding-left: 16px;
     display: flex;
     align-items: center;
@@ -40,7 +39,7 @@
     }
 
     input {
-      height: 64px;
+      height: 40px;
       flex-grow: 1;
       background: transparent;
       border: none;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,9 +2,11 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes file-name="备注"
-           placeholder="在这里输入备注"
-           @update:value="onUpdateNotes"/>
+    <div class="notes">
+      <FormItem file-name="备注"
+                placeholder="在这里输入备注"
+                @update:value="onUpdateNotes"/>
+    </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -13,7 +15,7 @@
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import { Component, Watch } from 'vue-property-decorator';
   import recordListModel from '../models/recordListModel';
@@ -23,7 +25,7 @@
   const tagList = tagListModel.fetch();
 
   @Component({
-    components: { Tags, Notes, Types, NumberPad }
+    components: { Tags, FormItem, Types, NumberPad }
   })
   export default class Money extends Vue {
     tags = tagList;
@@ -57,5 +59,8 @@
   .layout-content {
     display: flex;
     flex-direction: column-reverse;
+  }
+  .notes {
+    padding: 12px 0;
   }
 </style>
