36b5fcd1ae481f7559a9332659d2f9719844786f
zen-yang <519796642@qq.com>
2020-03-08 00:26:40 +0800

    将 create 封装到 model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -43,14 +43,12 @@
     }
 
     saveRecord() {
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
+      recordListModel.create(this.record);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
   }
 </script>
@@ -60,6 +58,7 @@
     display: flex;
     flex-direction: column-reverse;
   }
+
   .notes {
     padding: 12px 0;
   }
