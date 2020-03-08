e70d2c48ad53a521cae46c15fed0641e047a3661
zen-yang <519796642@qq.com>
2020-03-08 14:54:57 +0800

    封装 window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -23,7 +23,6 @@
   import tagListModel from '@/models/tagListModel';
   import Button from '@/components/Button.vue';
 
-
   @Component({
     components: { Button }
   })
@@ -33,12 +32,7 @@
     createTag() {
       const name = window.prompt('请输入标签名');
       if (name) {
-        const message = tagListModel.create(name);
-        if (message === 'duplicated') {
-          window.alert('标签名重复了');
-        } else if (message === 'success') {
-          window.alert('添加成功');
-        }
+        window.createTag(name);
       }
     }
   }
