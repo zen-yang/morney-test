479cbc586c597ba1ca13e3dfd4fda32bdbdafd31
zen-yang <519796642@qq.com>
2020-03-09 21:24:54 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,15 +19,17 @@
   @Component({
     computed: {
       tagList() {
-        // TODO
-        // return this.$store.fetchTags();
-        return []
+        return this.$store.state.tagList;
       }
     }
   })
   export default class Tags extends Vue {
 
     selectedTags: string[] = [];
+
+    created() {
+      this.$store.commit('fetchTags');
+    }
 
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
@@ -41,8 +43,7 @@
     create() {
       const name = window.prompt('请输入标签名');
       if (!name) {return window.alert('标签名不能为空');}
-      // TODO
-      // store.createTag(name);
+      this.$store.commit('createTag', name);
     }
   }
 </script>
