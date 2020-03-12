87107d79ca4829c7f8345c77b5c877f055a9ff81
zen-yang <519796642@qq.com>
2020-03-12 11:49:28 +0800

    完成列表的 CSS

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,11 +5,7 @@ import createId from '@/lib/createId';
 import router from '@/router';
 
 Vue.use(Vuex);
-type RootState = {
-  recordList: RecordItem[],
-  tagList: Tag[],
-  currentTag?: Tag
-}
+
 const store = new Vuex.Store({
   state: {
     recordList: [],
@@ -55,7 +51,7 @@ const store = new Vuex.Store({
     },
     createRecord(state, record) {
       const record2: RecordItem = clone(record);
-      record2.createdAt = new Date();
+      record2.createdAt = new Date().toISOString();
       state.recordList.push(record2);
       store.commit('saveRecords');
     },
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,11 +2,18 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-    <div>
-      type: {{type}}
-      <br/>
-      interval: {{interval}}
-    </div>
+    <ol>
+      <li v-for="(group, index) in result" :key="index">
+        <h3 class="title">{{group.title}}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -18,9 +25,33 @@
   import recordTypeList from '@/constants/recordTypeList';
 
   @Component({
-    components: { Tabs},
+    components: { Tabs },
   })
   export default class Statistics extends Vue {
+    tagString(tags: Tag[]) {
+      return tags.join()
+    }
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
+    }
+
+    get result() {
+      const { recordList } = this;
+      type HashTableValue = { title: string, items: RecordList[] }
+
+      const hashTable: { [key: string]: HashTableValue } = {};
+      for (let i = 0; i < recordList.length; i++) {
+        const [date, time] = recordList[i].createdAt!.split('T');
+        hashTable[date] = hashTable[date] || { title: date, items: [] };
+        hashTable[date].items.push(recordList[i]);
+      }
+      return hashTable;
+    }
+
+    beforeCreate() {
+      this.$store.commit('fetchRecords');
+    }
+
     type = '-';
     interval = 'day';
     intervalList = intervalList;
@@ -29,17 +60,40 @@
 </script>
 
 <style scoped lang="scss">
-  ::v-deep .type-tabs-item {
-    background: white;
+  ::v-deep {
+    .type-tabs-item {
+      background: white;
 
-    &.selected {
-      background: #c4c4c4;
-      &::after {
-        display: none;
+      &.selected {
+        background: #c4c4c4;
+
+        &::after {
+          display: none;
+        }
       }
     }
+
+    .interval-tabs-item {
+      height: 48px;
+    }
   }
-  ::v-deep .interval-tabs-item {
-    height: 48px;
+  %item {
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+  .title {
+    @extend %item;
+  }
+  .record {
+    background: white;
+    @extend %item;
+  }
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
   }
 </style>
